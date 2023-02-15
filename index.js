1c5a7a2ef71b6943d76ed2bf811679c7d58fa7d4
Evgeny Khodykin <evgeny.khodykin@yandex.ru>
2023-02-15 20:41:12 +0500

    added imports

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 import './index.css'
+import JS_IMAGE from './assets/JS.jpg'
 
 const h1 = document.createElement('h1')
 h1.textContent = 'I Love JavaScript'
 
 const image = document.createElement('img')
-image.src = './assets/JS.jpg'
+image.src = JS_IMAGE
 
 document.body.append(h1,image)
--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,8 @@
 import babel from "@rollup/plugin-babel"
 import styles from "rollup-plugin-styles"
+import image from "rollup-plugin-img"
+import serve from "rollup-plugin-serve"
+import livereload from "rollup-plugin-livereload"
 
 export default {
 	input: './index.js',
@@ -15,6 +18,15 @@ export default {
 			babelHelpers: "bundled",
 			presets: ["@babel/env"]
 		}),
-		styles()
+		styles(),
+		image({
+			limit:100000
+		}),
+		serve({
+			open: true,
+			contentBase: './',
+			port: 8000
+		}),
+		livereload()
 	]
 }
